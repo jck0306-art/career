85af83d119e769641f7576d48a8c84fd19a9164d
jck0306-art <jck0306@gmail.com>
2026-09-04 14:50:06 +0900

    Add escapeHTML function to sanitize HTML input

---
+++ js/security.js
@@ -0,0 +1,10 @@
+export function escapeHTML(str) {
+  if (str === null || str === undefined) return '';
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/`/g, '&#96;');
+}
